54b2d662a803ce9df41703b44c10badc3701b3f3
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 17:25:16 +0300

    Update 5.ETH_holders_DAI.py

--- 5.ETH_holders_DAI.py
+++ 5.ETH_holders_DAI.py
@@ -3,7 +3,7 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-display = Display(visible=0, size=(1920, 768))  
+display = Display(visible=0, size=(1920, 1080))  
 display.start()
 driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
@@ -12,8 +12,7 @@ menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
 
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
-
-wait(EC.element_to_be_clickable((By.CSS_SELECTOR, '.cdk-overlay-pane>.mat-menu-panel>.mat-menu-content>:nth-child(5)'))).click()
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.cdk-overlay-pane>.mat-menu-panel>.mat-menu-content>:nth-child(5)'))).click()
 
 #eth_holders = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(5)').click()
 owners_DAI = driver.find_element_by_css_selector('#cdk-overlay-1>:nth-child(1)>:nth-child(1)>:nth-child(1)').click()
