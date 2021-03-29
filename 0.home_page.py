b1c6130c7f2e14ea89dea114db19a57a7bff6cd9
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 12:43:47 +0300

    Update 0.home_page.py

--- 0.home_page.py
+++ 0.home_page.py
@@ -9,8 +9,12 @@ driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
 logo = driver.find_element_by_css_selector('.col-xl-2.col-lg-2.col-md-2.col-sm-6.col-6.logo_box').click()
-time.sleep(5)
-Guarantees = driver.find_element_by_css_selector('.mat-ripple.mat-button-ripple.mat-button-ripple-round').click()
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+gar = WebDriverWait(driver, 15).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, '.mat-ripple.mat-button-ripple.mat-button-ripple-round'))).click()
 
 balance_on_Etherscan = driver.find_element_by_link_text('Check balance on Etherscan').click()
 new_window = driver.window_handles[1]
