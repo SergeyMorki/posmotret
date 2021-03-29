374bd9b4ee251dc14f2f5867c5074422a3a39660
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 14:53:55 +0300

    Update 0.home_page.py

--- 0.home_page.py
+++ 0.home_page.py
@@ -18,8 +18,8 @@ wait = WebDriverWait(driver, 15).until
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger'))).click()
 
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.mat-menu-content>:nth-child(1)'))).click()
-
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
+assert driver.current_url == "https://etherscan.io/address/0x0bcbab2fecc30b7341132b4ebb36d352e035f1bd"
 driver.quit()
