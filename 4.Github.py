4f7ba790ee3c30cc042dd6afcf9cc0668157fb97
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 16:44:25 +0300

    Update 4.Github.py

--- 4.Github.py
+++ 4.Github.py
@@ -14,8 +14,9 @@ menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
 
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR, '.mat-menu-content>[href="https://github.com/DeFireX"]'))).click()
 
-github = driver.find_element_by_css_selector('.mat-menu-content>[href="https://github.com/DeFireX"]').click()
+#github = driver.find_element_by_css_selector('.mat-menu-content>[href="https://github.com/DeFireX"]').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
