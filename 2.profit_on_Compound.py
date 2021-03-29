667771eebaef7c5295dd0325292d7b6342a7986d
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 15:23:37 +0300

    Update 2.profit_on_Compound.py

--- 2.profit_on_Compound.py
+++ 2.profit_on_Compound.py
@@ -1,15 +1,20 @@
 import time
 
 from selenium import webdriver  # импортируем webdriver
-
-driver = webdriver.Chrome()
-driver.maximize_window()
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+display = Display(visible=0, size=(1920, 1080))  
+display.start()
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
-Guarantees = driver.find_element_by_css_selector('.mat-focus-indicator>.mat-button-wrapper')
-Guarantees.click()
-balance_on_Compound = driver.find_element_by_link_text('Check profit on Compound').click()
+wait = WebDriverWait(driver, 15).until
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger'))).click()
+
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.mat-menu-content>:nth-child(1)'))).click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
