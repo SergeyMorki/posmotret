f398d5a5839914e63c32ee7819b3a1257eb1ee34
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 16:01:11 +0300

    Update 4.Github.py

--- 4.Github.py
+++ 4.Github.py
@@ -14,7 +14,7 @@ driver.implicitly_wait(5)
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
-github = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(4)').click()
+github = driver.find_element_by_link_text('Source code on GitHub').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
