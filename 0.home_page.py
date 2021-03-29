a9abf76235270209d4dc8d0fafcc584254ed4902
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 13:49:38 +0300

    Update 0.home_page.py

--- 0.home_page.py
+++ 0.home_page.py
@@ -9,8 +9,8 @@ driver.maximize_window()
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
-logo = driver.find_element_by_css_selector('.col-xl-2.col-lg-2.col-md-2.col-sm-6.col-6.logo_box').click()
 
+logo = driver.find_element_by_css_selector('.col-xl-2.col-lg-2.col-md-2.col-sm-6.col-6.logo_box').click()
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -18,7 +18,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 gar = driver.find_element_by_css_selector('.menu>.mat-menu-trigger').click()
 
-balance_on_Etherscan = driver.find_element_by_link_text('Check balance on Etherscan').click()
+balance_on_Etherscan = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(1)').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
