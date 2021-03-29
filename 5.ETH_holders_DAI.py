1483bc54beb96ddc597767b3bb35885adb63fa90
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 16:37:38 +0300

    Update 5.ETH_holders_DAI.py

--- 5.ETH_holders_DAI.py
+++ 5.ETH_holders_DAI.py
@@ -3,17 +3,18 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-display = Display(visible=0, size=(1920, 1080))  
+display = Display(visible=0, size=(1920, 768))  
 display.start()
 driver1 = webdriver.Chrome("/usr/bin/chromedriver")
 driver = webdriver.Chrome()
 driver == driver1
 driver.get('https://defirex.org/')
-driver.implicitly_wait(5)
 
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
+
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
+
 eth_holders = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(5)').click()
 owners_DAI = driver.find_element_by_css_selector('#cdk-overlay-1>:nth-child(1)>:nth-child(1)>:nth-child(1)').click()
 new_window = driver.window_handles[1]
