76ab6afa3f1fac26415e047cb996b27c54597b6d
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 15:57:41 +0300

    Update 8.How_it_works.py

--- 8.How_it_works.py
+++ 8.How_it_works.py
@@ -1,14 +1,19 @@
-import time
-
 from selenium import webdriver  # импортируем webdriver
-
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+display = Display(visible=0, size=(1920, 1080))  
+display.start()
+driver1 = webdriver.Chrome("/usr/bin/chromedriver")
 driver = webdriver.Chrome()
-driver.maximize_window()
+driver == driver1
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
-Guarantees = driver.find_element_by_css_selector('.mat-focus-indicator>.mat-button-wrapper')
-Guarantees.click()
+menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
+wait = WebDriverWait(driver, 15).until
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
 How_it_works = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(7)').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
