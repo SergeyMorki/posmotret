7738ec6529b061c44f716b6baa8966c76b15f094
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 17:18:58 +0300

    Update 5.ETH_holders_DAI.py

--- 5.ETH_holders_DAI.py
+++ 5.ETH_holders_DAI.py
@@ -5,9 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 display = Display(visible=0, size=(1920, 768))  
 display.start()
-driver1 = webdriver.Chrome("/usr/bin/chromedriver")
-driver = webdriver.Chrome()
-driver == driver1
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
 
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
@@ -15,7 +13,9 @@ wait = WebDriverWait(driver, 15).until
 
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
 
-eth_holders = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(5)').click()
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR, '.mat-menu-content>:nth-child(5)'))).click()
+
+#eth_holders = driver.find_element_by_css_selector('.mat-menu-content>:nth-child(5)').click()
 owners_DAI = driver.find_element_by_css_selector('#cdk-overlay-1>:nth-child(1)>:nth-child(1)>:nth-child(1)').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
