230325537d5a21ea88512fb312992dbabde39ade
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-29 17:07:01 +0300

    Update 5.ETH_holders_DAI.py

--- 5.ETH_holders_DAI.py
+++ 5.ETH_holders_DAI.py
@@ -21,4 +21,5 @@ new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
 assert driver.current_url == "https://etherscan.io/token/0xfACd9A6fD887855d9432F7a080911b26d9DCAE01#balances"
+display.stop()
 driver.quit()
