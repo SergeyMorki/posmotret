12c20620b35074031ebc016114fba1000ce91036
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 16:52:07 +0300

    Update 16.Learn_how.py

--- 16.Learn_how.py
+++ 16.Learn_how.py
@@ -1,12 +1,13 @@
 import time
 
 from selenium import webdriver  # импортируем webdriver
-
-display = Display(visible=0, size=(1920, 1080))  
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+display = Display(visible=0, size=(1024, 768))
 display.start()
-driver1 = webdriver.Chrome("/usr/bin/chromedriver")
-driver = webdriver.Chrome()
-driver == driver1
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
