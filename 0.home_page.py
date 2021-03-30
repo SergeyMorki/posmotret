a430ed94b7c094133c673789a0d1b61e482a9ae1
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 12:57:42 +0300

    Update 0.home_page.py

--- 0.home_page.py
+++ 0.home_page.py
@@ -23,16 +23,6 @@ new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
 assert driver.current_url == "https://etherscan.io/address/0x0bcbab2fecc30b7341132b4ebb36d352e035f1bd"
-
-menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
-wait = WebDriverWait(driver, 15).until
-wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
-
-balance_on_Compound = driver.find_element_by_link_text('Check profit on Compound').click()
-new_window = driver.window_handles[1]
-current_window = driver.current_window_handle
-driver.switch_to.window(new_window)
-assert driver.current_url == "https://compound.finance/governance/address/0x0BCbAb2FeCC30B7341132B4Ebb36d352E035f1bD"
-display.popen.terminate()
+display.stop()
 driver.quit()
 
