837e9e12b9b39e24bc19930df1185324b897e6aa
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 17:16:41 +0300

    Update 25.footLink.py

--- 25.footLink.py
+++ 25.footLink.py
@@ -1,9 +1,13 @@
 import time
 
 from selenium import webdriver  # импортируем webdriver
-
-driver = webdriver.Chrome()
-driver.maximize_window()
+from pyvirtualdisplay import Display
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+display = Display(visible=0, size=(1024, 768))
+display.start()
+driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
