4b20417d1057e5cfc3443254eb976ecf7d4e00b4
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 16:33:00 +0300

    Update 13.balances.py

--- 13.balances.py
+++ 13.balances.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver  # импортируем webdriver
 
-display = Display(visible=0, size=(1920, 1080))  
+display = Display(visible=0, size=(1024, 768))  
 display.start()
 driver1 = webdriver.Chrome("/usr/bin/chromedriver")
 driver = webdriver.Chrome()
@@ -14,19 +14,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-system_balance = WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, '.statistic_box>.stat>:nth-child(1)>:nth-child(1)>.ng-star-inserted')))
-href = system_balance.get_attribute('href')
-time.sleep(3)
-assert href == 'https://etherscan.io/address/0x0bcbab2fecc30b7341132b4ebb36d352e035f1bd'
+users_balance = driver.find_element_by_css_selector('.mat-menu-trigger.ng-star-inserted').click()
+on_ETH = driver.find_element_by_css_selector('.cdk-overlay-pane>.mat-menu-panel>.mat-menu-content>:nth-child(1)')
+time.sleep(2)
+on_ETH.click()
+DAI_funds = driver.find_element_by_link_text('DAI funds holders')
+DAI_funds.click()
+new_window = driver.window_handles[1]
+current_window = driver.current_window_handle
+driver.switch_to.window(new_window)
+assert driver.current_url == "https://etherscan.io/token/0xfACd9A6fD887855d9432F7a080911b26d9DCAE01"
+driver.close()
+driver.switch_to.window(current_window)
 
-users_balance = driver.find_element_by_css_selector('.statistic_box>.stat>:nth-child(2)>:nth-child(1)>:nth-child(2)')
-href = users_balance.get_attribute('href')
-time.sleep(3)
-assert href == 'https://etherscan.io/token/0xfacd9a6fd887855d9432f7a080911b26d9dcae01#balances'
+users_balance = driver.find_element_by_css_selector('.mat-menu-trigger.ng-star-inserted').click()
+on_ETH = driver.find_element_by_css_selector('.cdk-overlay-pane>.mat-menu-panel>.mat-menu-content>:nth-child(1)')
+time.sleep(2)
+on_ETH.click()
+ETH_funds = driver.find_element_by_link_text('ETH funds holders')
+ETH_funds.click()
+new_window = driver.window_handles[1]
+current_window = driver.current_window_handle
+driver.switch_to.window(new_window)
+assert driver.current_url == "https://etherscan.io/token/0xF145A9e7Edc6D5a27BBdd16E4E29F5Fe56671A22"
 
-daily_profit = driver.find_element_by_css_selector('.statistic_box>.stat>:nth-child(3)>:nth-child(1)>:nth-child(2)')
-href = daily_profit.get_attribute('href')
-time.sleep(3)
-assert href == 'https://compound.finance/governance/address/0x0BCbAb2FeCC30B7341132B4Ebb36d352E035f1bD'
 driver.quit()
