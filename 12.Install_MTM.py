8b3ec4f6e4a41426bcbf683b4df4e02a2a22ea07
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 16:31:51 +0300

    Update 12.Install_MTM.py

--- 12.Install_MTM.py
+++ 12.Install_MTM.py
@@ -2,7 +2,7 @@ import time
 
 from selenium import webdriver  # импортируем webdriver
 
-display = Display(visible=0, size=(1920, 1080))  
+display = Display(visible=0, size=(1024, 768))  
 display.start()
 driver1 = webdriver.Chrome("/usr/bin/chromedriver")
 driver = webdriver.Chrome()
