8d7c1b55f8427160f42c60e7ecf7d78957428009
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 12:56:17 +0300

    Update 0.home_page.py

--- 0.home_page.py
+++ 0.home_page.py
@@ -5,7 +5,7 @@ from pyvirtualdisplay import Display
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-display = Display(backend='Xephyr')
+display = Display(visible=0, size=(1024, 768))
 display.start()
 driver = webdriver.Chrome("/usr/bin/chromedriver")
 driver.get('https://defirex.org/')
@@ -23,5 +23,16 @@ new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
 driver.switch_to.window(new_window)
 assert driver.current_url == "https://etherscan.io/address/0x0bcbab2fecc30b7341132b4ebb36d352e035f1bd"
+
+menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
+wait = WebDriverWait(driver, 15).until
+wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
+
+balance_on_Compound = driver.find_element_by_link_text('Check profit on Compound').click()
+new_window = driver.window_handles[1]
+current_window = driver.current_window_handle
+driver.switch_to.window(new_window)
+assert driver.current_url == "https://compound.finance/governance/address/0x0BCbAb2FeCC30B7341132B4Ebb36d352E035f1bD"
 display.popen.terminate()
 driver.quit()
+
