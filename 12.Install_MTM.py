523688602073d801bc258deac4a09343d7017263
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 16:54:14 +0300

    Update 12.Install_MTM.py

--- 12.Install_MTM.py
+++ 12.Install_MTM.py
@@ -12,5 +12,6 @@ driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
 Install_MTM = driver.find_element_by_id('Open_Modal_install_Metamask').click()
+time.sleep(2)
 close_alert_mtm = driver.find_element_by_css_selector('.metamask_Content>[type="button"]').click()
 driver.quit()
