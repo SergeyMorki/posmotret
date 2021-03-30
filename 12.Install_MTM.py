8f28eff47d517f3c778bf5b44a419c0a7bbd80fc
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 16:39:38 +0300

    Update 12.Install_MTM.py

--- 12.Install_MTM.py
+++ 12.Install_MTM.py
@@ -12,5 +12,5 @@ driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
 
 Install_MTM = driver.find_element_by_id('Open_Modal_install_Metamask').click()
-close_alert_mtm = driver.find_element_by_css_selector('.mat-focus-indicator.mat-button.mat-button-base>.mat-button-wrapper').click()
+close_alert_mtm = driver.find_element_by_css_selector('.metamask_Content>[type="button"]').click()
 driver.quit()
