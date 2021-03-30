34811b3a945fb1550976315c50c1429497daabdb
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-03-30 17:30:46 +0300

    Update 23.FAQ_4.py

--- 23.FAQ_4.py
+++ 23.FAQ_4.py
@@ -13,6 +13,7 @@ driver.implicitly_wait(5)
 
 time.sleep(1)
 How_profit = driver.find_element_by_id('mat-expansion-panel-header-3').click()
+time.sleep(2)
 balance_on_compound = driver.find_element_by_id('Balance_on_Compound').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
