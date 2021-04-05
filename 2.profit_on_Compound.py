de8df0887e7aea7d91a1d4c8fe2e7e76b2dc1770
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-04-05 11:39:49 +0300

    Update 2.profit_on_Compound.py

--- 2.profit_on_Compound.py
+++ 2.profit_on_Compound.py
@@ -12,11 +12,9 @@ driver = webdriver.Chrome()
 driver == driver1
 driver.get('https://defirex.org/')
 driver.implicitly_wait(10)
-
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
-
 balance_on_Compound = driver.find_element_by_link_text('Check profit on Compound').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
