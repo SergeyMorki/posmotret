0889b4494bbf9a3621d9480d2da511c88a2b0bdb
SergeyMorki <77924687+SergeyMorki@users.noreply.github.com>
2021-04-07 13:50:36 +0300

    Update 2.profit_on_Compound.py

--- 2.profit_on_Compound.py
+++ 2.profit_on_Compound.py
@@ -13,6 +13,7 @@ driver.implicitly_wait(10)
 menu = driver.find_element_by_css_selector('.slog>.mobile_button').click()
 wait = WebDriverWait(driver, 15).until
 wait(EC.element_to_be_clickable((By.CSS_SELECTOR,'.menu>.mat-menu-trigger>.warranty'))).click()
+time.sleep(1)
 balance_on_Compound = driver.find_element_by_link_text('Check profit on Compound').click()
 new_window = driver.window_handles[1]
 current_window = driver.current_window_handle
