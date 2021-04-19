2fe60a6084a9a4fbda8093407937c10da30ac586
tdallstr@gmail.com <123654Qw/>
2021-04-19 17:51:44 +0300

    go

--- 5.pools.py
+++ 5.pools.py
@@ -7,7 +7,7 @@ driver.maximize_window()
 
 driver.get('https://defirex.org/')
 driver.implicitly_wait(5)
-#pools = .pools.curr1.ng-star-inserted
+
 pool = driver.find_element_by_css_selector('.pools.curr1.ng-star-inserted>:nth-child(2)').click()
 input2 = driver.find_element_by_css_selector('.active>.moreInfo>.dep>.mat-tab-group>.mat-tab-body-wrapper>.mat-tab-body-active>:nth-child(1)>:nth-child(2)>.input>:nth-child(1)')
 input2.send_keys('123')
